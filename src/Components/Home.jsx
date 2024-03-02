f2f1962a23264814ffb4a561ac8678ac3328abec
Richard <olugbenga.rich@gmail.com>
2024-03-02 09:36:35 +0100

    It begins.

---
+++ src/Components/Home.jsx
@@ -0,0 +1,42 @@
+
+
+
+const Home = () => {
+
+  return (
+    <>
+    <div className='header'>
+
+    <div className="bars">
+
+    <div className="cover">Entertainment Hub</div>
+
+    <form>
+            <div className='search-button'>
+                <input type='text' 
+                placeholder='Enter Movie Name' 
+                className='inputText' >
+                </input>
+                <button>
+                <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </form>
+
+        </div>
+
+        
+    </div>
+        
+        
+        
+    <div className='container'>
+       
+    </div>
+    </>
+  )
+}
+
+export default Home;
+
+
