f9c8e2dfc031cd2c45fe187341d35e3de5da279f
Richard <olugbenga.rich@gmail.com>
2024-03-02 09:36:57 +0100

    Setting up Api's

---
+++ src/Components/Movie.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//https://api.themoviedb.org/3/movie/157336?api_key=REACT_APP_API_KEY
+const Movie = () => {
+    return(
+        <div>
+            Movie
+        </div>
+    )
+}
---
+++ src/Components/MovieList.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+//'https://api.themoviedb.org/3/movie/popular?api_key=REACT_APP_API_KEY'
+//'https://api.themoviedb.org/3/trending/person/day?api_key=REACT_APP_API_KEY'
+//'https://api.themoviedb.org/3/genre/tv/list?api_key=REACT_APP_API_KEY'
+const MovieCard = () => {
+    return(
+        <div>
+            List
+        </div>
+    )
+}
